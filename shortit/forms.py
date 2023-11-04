fba06eb8c40f36d0b9641af01569b0e46104e079
Fauqi Amar Hizbullah <a@fauqi.com>
2023-11-04 14:15:57 +0700

    Update the home page

--- shortit/forms.py
+++ shortit/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
 class URLForm(forms.Form):
-    url = forms.URLField(label='URL to shorten')
+    url = forms.URLField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your long URL here...'
+        }),
+        error_messages={
+            'invalid': 'Invalid URL. Please enter a valid URL.'
+        }
+    )
